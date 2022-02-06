2c38d815582f168d923b87b7341114fde97c540a
Gabriel Paz Paiva <gabripaiva@hotmail.com>
2022-02-06 12:32:03 -0400

    Speaking a text

--- PrjOctoAndroidApp/app/src/main/java/com/example/prjoctoandroidapp/MainActivity.java
+++ PrjOctoAndroidApp/app/src/main/java/com/example/prjoctoandroidapp/MainActivity.java
@@ -3,12 +3,65 @@ package com.example.prjoctoandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextToSpeech textToSpeech;
+    private EditText edText;
+    private Button btnSpeech;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intialize();
+    }
+
+    private void intialize() {
+        btnSpeech = findViewById(R.id.btnSpeak);
+        edText = findViewById(R.id.edTextToSay);
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if(i == TextToSpeech.SUCCESS){ //validates the initialization
+                    int res = textToSpeech.setLanguage(Locale.ENGLISH);
+                    switch (res){
+                        case TextToSpeech.LANG_MISSING_DATA:
+                        case TextToSpeech.LANG_NOT_SUPPORTED:
+                            Log.e("Text to Speech", "Language not supported");
+                            break;
+                        default:
+                            btnSpeech.setEnabled(true);
+                    }
+                }else{
+                    Log.e("Text to Speech", "Initialization Failed");
+                }
+            }
+        });
+        btnSpeech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                speak();
+            }
+        });
+    }
+
+    private void speak() {
+        String message = edText.getText().toString();
+        textToSpeech.speak(message, TextToSpeech.QUEUE_FLUSH, null);
+    }
+    //just for security
+    @Override
+    protected void onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
     }
 }

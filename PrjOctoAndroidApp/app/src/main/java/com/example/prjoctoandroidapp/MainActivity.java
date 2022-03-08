c0189cc75394a265c760dccda9b9738326629dcd
Gabriel Paz Paiva <gabripaiva@hotmail.com>
2022-03-08 10:25:20 -0400

    Comment to be implemented.

--- PrjOctoAndroidApp/app/src/main/java/com/example/prjoctoandroidapp/MainActivity.java
+++ PrjOctoAndroidApp/app/src/main/java/com/example/prjoctoandroidapp/MainActivity.java
@@ -105,6 +105,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void goStart() {
         Intent intent = new Intent(this,PlaygroundActivity.class);
         //We need to put in the intent the id from the profile to manage easily later
+
         //intent.putExtra("id":id)
         startActivity(intent);
     }
